54c219bcad58e70f252177d95f793d70a681fa36
AskanHolaAE17 <be.in.pair@gmail.com>
2020-02-26 13:47:00 +0200

    Realized dynamical questions adding

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery 
+
 //= require activestorage
 //= require turbolinks
-//= require cocoon
 //= require_tree .
 
 $(document).on('turbolinks:load', function() {
